c69e88d2a307e02c326401158367478f13a0ce33
Stephen Fluin <stephen.fluin@gmail.com>
2023-08-16 09:14:59 -0500

    fix: add missing sample back

---
+++ public/samples/interchain-token-simple.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract InterchainToken is ERC20 {
+
+    constructor() ERC20("MyInterchainToken", "MITKN") {
+        // Mint 1000 tokens to the creator
+        _mint(msg.sender, 1000 * 10 ** 18);
+    }
+}
