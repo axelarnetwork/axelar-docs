83ccae48ebd05bef8c6f15e1bf67974f4f27aead
Stephen Fluin <stephen.fluin@gmail.com>
2023-09-18 00:00:28 -0500

    fix: keplr button color
    Fixes #560

--- src/components/keplr/index.jsx
+++ src/components/keplr/index.jsx
@@ -1,51 +1,49 @@
-export default ({
-  environment = "mainnet",
-  chain,
-}) => {
-
+export default ({ environment = "mainnet", chain }) => {
   const addNetwork = async (settingsPromise) => {
     try {
       const settings = await settingsPromise;
+      try {
+        await window.keplr.enable(settings.chainId);
+        alert(settings.chainId + " already added");
+      } catch (e) {
+        console.log(
+          "Unable to connect to wallet natively, so trying experimental chain"
+        );
         try {
-            await window.keplr.enable(settings.chainId)
-            alert(settings.chainId + " already added")
-        } catch (e) {
-            console.log("Unable to connect to wallet natively, so trying experimental chain")
-            try {
-                await window.keplr.experimentalSuggestChain(settings)
-                await window.keplr.enable(settings.chainId)
-            } catch (e2) {
-                console.log("and yet there is a problem in trying to do that too", e2)
-            }
+          await window.keplr.experimentalSuggestChain(settings);
+          await window.keplr.enable(settings.chainId);
+        } catch (e2) {
+          console.log(
+            "and yet there is a problem in trying to do that too",
+            e2
+          );
         }
-
-    }
-    catch (error) {
-        if (error instanceof SyntaxError) {
-            alert("There was a syntax error. Please correct it and try again: " + error.message);
-        }
-        else {
-            throw error;
-        }
+      }
+    } catch (error) {
+      if (error instanceof SyntaxError) {
+        alert(
+          "There was a syntax error. Please correct it and try again: " +
+            error.message
+        );
+      } else {
+        throw error;
+      }
     }
   };
 
   return (
     <button
       onClick={() => {
-        console.log("network and chain",environment,chain)
-        const settings = import(`../../data/keplr/${environment}/${chain}.json`);
-        console.log('got back settings',settings);
+        console.log("network and chain", environment, chain);
+        const settings = import(
+          `../../data/keplr/${environment}/${chain}.json`
+        );
+        console.log("got back settings", settings);
         addNetwork(settings);
       }}
-      className="min-w-max hover:bg-gray-200 dark:bg-gray-900 dark:hover:bg-gray-800 rounded-lg cursor-pointer flex items-center py-1.5 px-2"
+      className="flex keplr-button"
     >
-      <img
-        src="/images/wallets/keplr.png"
-        alt=""
-        width={16}
-        height={16}
-      />
+      <img src="/images/wallets/keplr.png" alt="" width={16} height={16} />
     </button>
   );
 };
