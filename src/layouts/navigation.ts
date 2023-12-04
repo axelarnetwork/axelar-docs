9abcccd28e80ed51af53b2e2b55452e9c01b95e4
Juan Palacios <juan.palacios@immutable.com>
2023-12-04 15:17:58 +1100

    Created a page to document the setup of an Immutable zkEVM partner node

--- src/layouts/navigation.ts
+++ src/layouts/navigation.ts
@@ -213,6 +213,7 @@ export const getNavigation = (section) => {
             { title: "Ethereum", href: "/validator/external-chains/ethereum" },
             { title: "Fantom", href: "/validator/external-chains/fantom" },
             { title: "Filecoin", href: "/validator/external-chains/filecoin" },
+            { title: "Immutable zkEVM", href: "/validator/external-chains/immutable"},
             { title: "Kava", href: "/validator/external-chains/kava" },
             { title: "Linea", href: "/validator/external-chains/linea" },
             { title: "Mantle", href: "/validator/external-chains/mantle" },
