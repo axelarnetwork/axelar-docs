3c8149627e15cb6def6752ea4f8078caeb417af1
Hamzat Victor <oluwaborihamzat@gmail.com>
2023-12-07 10:09:48 +0100

    feat: update menu behavior and enable scroll disablement
    The menu behavior has been updated to use querySelectorAll and forEach
    to add event listeners to all menu buttons. When a menu button is
    clicked, the mobile navigation menu (mobile-nav) toggles its display
    using the "show" class, and the scroll on the body is disabled by
    toggling the "overflow-hidden" class on the document body. This change
    improves the user experience by ensuring consistent and intuitive menu
    interaction on various devices.

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,3 +1,10 @@
-document.getElementById("menu").addEventListener("click", function () {
-  document.querySelector(".sideNav").classList.toggle("show");
+const menu = document.querySelectorAll(".menu-btn");
+const mobileNav = document.querySelector(".mobile-nav");
+
+menu.forEach((item) => {
+  item?.addEventListener("click", function () {
+    mobileNav?.classList.toggle("show");
+    // disable scroll on body
+    document.body.classList.toggle("overflow-hidden");
+  });
 });
