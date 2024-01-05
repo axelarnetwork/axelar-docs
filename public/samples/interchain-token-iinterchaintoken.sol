711654c3539286db94895a4627a52b2e8e70a430
Idris Olubisi <heedris2olubisi@gmail.com>
2024-01-05 16:20:45 +0100

    fix: update ITS code sample token

--- public/samples/interchain-token-iinterchaintoken.sol
+++ public/samples/interchain-token-iinterchaintoken.sol
@@ -19,8 +19,9 @@ contract MyInterchainToken is ERC20, Ownable {
     using AddressBytesUtils for address;
 
     ITokenManager public tokenManager;
+
     IInterchainTokenService public service =
-        IInterchainTokenService(0xF786e21509A9D50a9aFD033B5940A2b7D872C208);
+        IInterchainTokenService(0xB5FB4BE02232B1bBA4dC8f81dc24C26980dE9e3C);
 
     address public creator;
 
@@ -43,17 +44,26 @@ contract MyInterchainToken is ERC20, Ownable {
     }
 
     function deployTokenManager(bytes32 salt) internal {
-        bytes memory params = service.getParamsMintBurn(
+
+        bytes memory params = tokenManager.params(
             msg.sender.toBytes(),
             address(this)
         );
-        bytes32 tokenId = service.deployCustomTokenManager(
+
+        service.deployTokenManager(
             salt,
+            "", // This is empty since the token manager is on the local chain but you need to specify it when you deploy on remote chain
             ITokenManagerType.TokenManagerType.MINT_BURN,
-            params
+            params,
+            msg.value // gasValue
         );
-        tokenManager = ITokenManager(service.getTokenManagerAddress(tokenId));
-        transferOwnership(address(tokenManager));
+
+        bytes32 tokenId = service.interchainTokenId(
+            msg.sender,
+            salt
+        );
+
+        tokenManager = ITokenManager(service.tokenManagerAddress(tokenId));
     }
 
     function myCustomFunction() public {
@@ -62,53 +72,47 @@ contract MyInterchainToken is ERC20, Ownable {
         _mint(creator, 1000 * 10**18);
     }
 
-    /*
-     * Deploy this token, then register it with the Interchain Token Service
-     * You'll be given a TokenManager which you can set here, allowing the
-     * local send methods to function.
-     */
-    function setTokenManager(address _tokenManager) public onlyOwner {
-        tokenManager = ITokenManager(_tokenManager);
-    }
-
     /**
-     * @notice Implementation of the interchainTransfer method
-     * @dev We chose to either pass `metadata` as raw data on a remote contract call, or, if no data is passed, just do a transfer.
-     * A different implementation could have `metadata` that tells this function which function to use or that it is used for anything else as well.
-     * @param destinationChain The destination chain identifier.
-     * @param recipient The bytes representation of the address of the recipient.
-     * @param amount The amount of token to be transfered.
-     * @param metadata Either empty, to just facilitate an interchain transfer, or the data can be passed for an interchain contract call with transfer as per semantics defined by the token service.
+     * @notice Initiates an interchain transfer of a specified token to a destination chain.
+     * @dev The function retrieves the TokenManager associated with the tokenId.
+     * @param tokenId The unique identifier of the token to be transferred.
+     * @param destinationChain The destination chain to send the tokens to.
+     * @param destinationAddress The address on the destination chain to send the tokens to.
+     * @param amount The amount of tokens to be transferred.
+     * @param metadata Optional metadata for the call for additional effects (such as calling a destination contract).
      */
     function interchainTransfer(
+        bytes tokenId,
         string calldata destinationChain,
         bytes calldata recipient,
         uint256 amount,
-        bytes calldata metadata
+        bytes calldata metadata,
+        uint256 gasValue
     ) external payable {
         address sender = msg.sender;
 
-        // Metadata semantics are defined by the token service and thus should be passed as-is.
-        tokenManager.transmitInterchainTransfer{value: msg.value}(
-            sender,
+        service.transmitInterchainTransfer{ value: msg.value}(
+            tokenId,
             destinationChain,
             recipient,
             amount,
-            metadata
+            metadata,
+            gasValue
         );
     }
 
     /**
      * @notice Implementation of the interchainTransferFrom method
      * @dev We chose to either pass `metadata` as raw data on a remote contract call, or, if no data is passed, just do a transfer.
-     * A different implementation could have `metadata` that tells this function which function to use or that it is used for anything else as well.
-     * @param sender the sender of the tokens. They need to have approved `msg.sender` before this is called.
-     * @param destinationChain the string representation of the destination chain.
-     * @param recipient the bytes representation of the address of the recipient.
-     * @param amount the amount of token to be transfered.
-     * @param metadata either empty, to just facilitate a cross-chain transfer, or the data to be passed to a cross-chain contract call and transfer.
+     * A different implementation could use metadata to specify a function to invoke, or for other purposes as well.
+     * @param sender The sender of the tokens. They need to have approved `msg.sender` before this is called.
+     * @param destinationChain The string representation of the destination chain.
+     * @param recipient The bytes representation of the address of the recipient.
+     * @param amount The amount of token to be transferred.
+     * @param metadata Optional metadata for the call for additional effects (such as calling a destination contract.)
      */
     function interchainTransferFrom(
+        bytes tokenId,
         address sender,
         string calldata destinationChain,
         bytes calldata recipient,
@@ -121,12 +125,13 @@ contract MyInterchainToken is ERC20, Ownable {
             _approve(sender, msg.sender, _allowance - amount);
         }
 
-        tokenManager.transmitInterchainTransfer{value: msg.value}(
-            sender,
+        service.transmitInterchainTransfer{ value: msg.value}(
+            tokenId,
             destinationChain,
             recipient,
             amount,
-            metadata
+            metadata,
+            gasValue
         );
     }
 }
