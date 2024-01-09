f9cfed8711708279f4cdf82934c7a3ea4676ed9d
Idris Olubisi <heedris2olubisi@gmail.com>
2024-01-09 18:36:26 +0100

    fix: update docs sample code contract

--- public/samples/interchain-token-iinterchaintoken.sol
+++ public/samples/interchain-token-iinterchaintoken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ITokenManager} from "@axelar-network/interchain-token-service/contracts/interfaces/ITokenManager.sol";
 import {ITokenManagerType} from "@axelar-network/interchain-token-service/contracts/interfaces/ITokenManagerType.sol";
 import {IInterchainToken} from "@axelar-network/interchain-token-service/contracts/interfaces/IInterchainToken.sol";
@@ -15,35 +16,38 @@ import {AddressBytesUtils} from "@axelar-network/interchain-token-service/contra
  * and security of your smart contracts before using
  * in production.
  */
-contract MyInterchainToken is ERC20, Ownable {
+contract MyInterchainToken is ERC20, AccessControl {
     using AddressBytesUtils for address;
 
-    ITokenManager public tokenManager;
+    address public creator;
+    bytes32 public tokenId;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+    ITokenManager public tokenManager;
     IInterchainTokenService public service =
         IInterchainTokenService(0xB5FB4BE02232B1bBA4dC8f81dc24C26980dE9e3C);
-
-    address public creator;
 
     constructor() ERC20("MyInterchainToken", "MITKN") {
         // Mint 1,000 tokens to the creator
         creator = msg.sender;
         _mint(creator, 1000 * 10**18);
+        _grantRole(MINTER_ROLE, msg.sender);
 
         // Register this token (could also be done 1-time smart contract invocation)
-        // Not a good practice beacuse it can't go to non-EVM chains
+        // Not a good practice because it can't go to non-EVM chains
         deployTokenManager("");
     }
 
-    function mint(address to, uint256 amount) external onlyOwner {
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    function burn(address from, uint256 amount) external onlyOwner {
+    function burn(address from, uint256 amount) external onlyRole(MINTER_ROLE) {
         _burn(from, amount);
     }
 
     function deployTokenManager(bytes32 salt) internal {
+        require(msg.value > 0, "Gas payment is required");
 
         bytes memory params = tokenManager.params(
             msg.sender.toBytes(),
@@ -52,18 +56,16 @@ contract MyInterchainToken is ERC20, Ownable {
 
         service.deployTokenManager(
             salt,
-            "", // This is empty since the token manager is on the local chain but you need to specify it when you deploy on remote chain
+            "", // This field remains empty if the token manager is deployed on the local chain; however, you need to specify it when deploying on a remote chain.
             ITokenManagerType.TokenManagerType.MINT_BURN,
             params,
             msg.value // gasValue
         );
 
-        bytes32 tokenId = service.interchainTokenId(
+        tokenId = service.interchainTokenId(
             msg.sender,
             salt
         );
-
-        tokenManager = ITokenManager(service.tokenManagerAddress(tokenId));
     }
 
     function myCustomFunction() public {
@@ -72,32 +74,36 @@ contract MyInterchainToken is ERC20, Ownable {
         _mint(creator, 1000 * 10**18);
     }
 
+    function transferMintership(address newMinter) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        
+        _revokeRole(MINTER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, newMinter);
+    }
+
     /**
-     * @notice Initiates an interchain transfer of a specified token to a destination chain.
-     * @dev The function retrieves the TokenManager associated with the tokenId.
-     * @param tokenId The unique identifier of the token to be transferred.
-     * @param destinationChain The destination chain to send the tokens to.
-     * @param destinationAddress The address on the destination chain to send the tokens to.
-     * @param amount The amount of tokens to be transferred.
+     * @notice Implementation of the interchainTransfer method.
+     * @dev We chose to either pass `metadata` as raw data on a remote contract call, or if no data is passed, just do a transfer.
+     * A different implementation could use metadata to specify a function to invoke, or for other purposes as well.
+     * @param destinationChain The destination chain identifier.
+     * @param recipient The bytes representation of the address of the recipient.
+     * @param amount The amount of token to be transferred.
      * @param metadata Optional metadata for the call for additional effects (such as calling a destination contract).
      */
     function interchainTransfer(
-        bytes tokenId,
         string calldata destinationChain,
         bytes calldata recipient,
         uint256 amount,
-        bytes calldata metadata,
-        uint256 gasValue
+        bytes calldata metadata
     ) external payable {
-        address sender = msg.sender;
 
-        service.transmitInterchainTransfer{ value: msg.value}(
+        service.transmitInterchainTransfer{ value: msg.value }(
             tokenId,
+            address(this),
             destinationChain,
             recipient,
             amount,
-            metadata,
-            gasValue
+            metadata
         );
     }
 
@@ -105,7 +111,6 @@ contract MyInterchainToken is ERC20, Ownable {
      * @notice Implementation of the interchainTransferFrom method
      * @dev We chose to either pass `metadata` as raw data on a remote contract call, or, if no data is passed, just do a transfer.
      * A different implementation could use metadata to specify a function to invoke, or for other purposes as well.
-     * @param tokenId The unique identifier of the token to be transferred.
      * @param sender The sender of the tokens. They need to have approved `msg.sender` before this is called.
      * @param destinationChain The string representation of the destination chain.
      * @param recipient The bytes representation of the address of the recipient.
@@ -113,7 +118,6 @@ contract MyInterchainToken is ERC20, Ownable {
      * @param metadata Optional metadata for the call for additional effects (such as calling a destination contract.)
      */
     function interchainTransferFrom(
-        bytes tokenId,
         address sender,
         string calldata destinationChain,
         bytes calldata recipient,
@@ -126,13 +130,13 @@ contract MyInterchainToken is ERC20, Ownable {
             _approve(sender, msg.sender, _allowance - amount);
         }
 
-        service.transmitInterchainTransfer{ value: msg.value}(
+       service.transmitInterchainTransfer{ value: msg.value }(
             tokenId,
+            address(this),
             destinationChain,
             recipient,
             amount,
-            metadata,
-            gasValue
+            metadata
         );
     }
 }
