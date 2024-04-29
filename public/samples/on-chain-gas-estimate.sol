365f402a7354513af9af60ab08bfe9439feada4f
Stephen Fluin <stephen.fluin@gmail.com>
2024-04-29 06:53:28 -0500

    fix: on-chain gas params and un-needed estimator
    improves #949

--- public/samples/on-chain-gas-estimate.sol
+++ public/samples/on-chain-gas-estimate.sol
@@ -1,19 +1,33 @@
 pragma solidity ^0.8.12;
 
-import { AxelarExecutable } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol';
-import { IAxelarGateway } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol';
-import { IAxelarGasService } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol';
-import { IInterchainGasEstimation } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IInterchainGasEstimation.sol';
-
+import {AxelarExecutable} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
+import {IAxelarGateway} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol";
+import {IAxelarGasService} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
 
 contract SenderReceiver is AxelarExecutable {
     IAxelarGasService public immutable gasService;
-    IInterchainGasEstimation public immutable estimator;
     string public message;
+    uint256 gasLimit = 200000;
 
-    constructor(address gateway_, address gasService_) AxelarExecutable(gateway_) {
+    constructor(address gateway_, address gasService_)
+        AxelarExecutable(gateway_)
+    {
         gasService = IAxelarGasService(gasService_);
-        estimator = IInterchainGasEstimation(gasService_);
+    }
+
+    function getEstimate(
+        string calldata destinationChain,
+        string calldata destinationAddress,
+        bytes memory payload
+    ) public view returns (uint256) {
+        return
+            gasService.estimateGasFee(
+                destinationChain,
+                destinationAddress,
+                payload,
+                gasLimit,
+                ""
+            );
     }
 
     function sendMessage(
@@ -22,25 +36,28 @@ contract SenderReceiver is AxelarExecutable {
         string calldata message_
     ) external payable {
         bytes memory payload = abi.encode(message_);
-        
-        uint256 estimate = estimator.estimateGasFee(destinationChain, destinationAddress, payload, 200000, '');
 
+        uint256 estimate = getEstimate(
+            destinationChain,
+            destinationAddress,
+            payload
+        );
 
-        gasService.payGas{value: estimate} (
+        gasService.payGas{value: estimate}(
             address(this),
             destinationChain,
             destinationAddress,
             payload,
-            200000,
-            true,
-            msg.sender,
-            ''
+            gasLimit, // uint256 executionGasLimit - destination gas limit
+            true, // bool estimateOnChain - was this gas estimated on-chain?
+            msg.sender, // address refundAddress
+            "" // bytes params
         );
 
         uint256 refund = msg.value - estimate;
         payable(msg.sender).transfer(refund);
 
-        gateway.callContract(destinationChain,destinationAddress,payload);
+        gateway.callContract(destinationChain, destinationAddress, payload);
     }
 
     function _execute(
@@ -51,11 +68,5 @@ contract SenderReceiver is AxelarExecutable {
         message = abi.decode(payload_, (string));
     }
 
-    function getEstimate(
-        string calldata destinationChain,
-        string calldata destinationAddress,
-        string calldata message_) external view returns (uint256) {
-            bytes memory payload = abi.encode(message_);
-        return estimator.estimateGasFee(destinationChain, destinationAddress, payload, 200000, '');
-    }
+    
 }
