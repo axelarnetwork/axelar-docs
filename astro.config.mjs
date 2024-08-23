59183b3534b31d198fbabf3a40469b148dec0b22
Stephen Fluin <stephen.fluin@gmail.com>
2024-08-23 15:03:46 -0500

    Add trailing slashes (#1121)

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,7 @@ function setDefaultLayout() {
 export default defineConfig({
   integrations: [react(), mdx(), sitemap()],
   site: "https://docs.axelar.dev",
+  trailingSlash: "always",
   markdown: {
     remarkPlugins: [
       setDefaultLayout,
