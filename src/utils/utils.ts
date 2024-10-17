a1487cf799ab9176014dd08fb29593a03d3d362c
dharamveergit <dharamveersingh.em@gmail.com>
2024-10-17 22:40:46 +0530

    delete:unused code

--- src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { Navigation } from "./generateNavigation";
-
-export const getUrl = (nav: Navigation) => {
-  if (nav?.href) {
-    return nav.href;
-  }
-
-  if (nav?.children?.[0]) return getUrl(nav?.children?.[0]);
-};
