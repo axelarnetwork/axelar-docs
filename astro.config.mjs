12a061093fee9bf91a9168d74537bd52079eacba
Stephen Fluin <stephen.fluin@gmail.com>
2024-12-03 11:51:31 -0600

    feat: add intercom

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,10 @@ export default defineConfig({
     }),
   ],
   site: "https://docs.axelar.dev",
-
+  trailingSlash: "always",
+  devToolbar: {
+    enabled: false,
+  },
   markdown: {
     remarkPlugins: [
       remarkDirective,
