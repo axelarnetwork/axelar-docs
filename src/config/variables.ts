5d6adbe3c5df5bb13bd790ddc97e7b0f6377b80c
ben-weinberg <weinbergben33@gmail.com>
2024-12-12 15:25:02 +0000

    chore: added correct axelard and ampd versions for config vars (#1272)

--- src/config/variables.ts
+++ src/config/variables.ts
@@ -1,5 +1,5 @@
 export const AXELARD = "1.0.5";
-export const AXELARD_TESTNET = "1.0.5";
-export const AMPD = "1.2.0";
+export const AXELARD_TESTNET = "1.1.2";
+export const AMPD = "1.3.1";
 export const TOFND = "1.0.1";
 export const TOFND_TESTNET = "1.0.1";
