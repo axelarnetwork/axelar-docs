b0817b58b0d90bfe9905023696cc40797d2c76ec
benjamin852 <weinbergben33@gmail.com>
2025-03-29 18:17:36 -0400

    doc: added gmp, its wip

--- src/layouts/navigation.ts
+++ src/layouts/navigation.ts
@@ -102,6 +102,11 @@ export const getNavigation = (section) => {
                     },
                   ],
                 },
+                //leaving xrpl here for now but we'll need to refactor this navigation
+                {
+                  title: "XRPL Interchain Token Service",
+                  href: "/dev/general-message-passing/xrpl/xrpl-gmp/#send-tokens",
+                },
               ],
             },
             {
@@ -285,11 +290,19 @@ export const getNavigation = (section) => {
                   title: "Send Tokens",
                   href: "/dev/send-tokens/deprecated/send-tokens/",
                 },
-                {
-                  title: "Deposit Address",
-                  href: "/dev/send-tokens/deprecated/deposit-address/",
-                },
-              ],
+              ]
+            },
+            {
+              title: "XRPL GMP",
+              href: "/dev/general-message-passing/xrpl/xrpl-gmp/",
+            },
+            {
+              title: "Solidity Utilities",
+              href: "/dev/solidity-utilities/",
+            },
+            {
+              title: "Sandbox",
+              href: "/dev/axelar-sandbox/intro/",
             },
           ],
         },
