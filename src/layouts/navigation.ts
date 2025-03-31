8679a082eac16bc0951d396260666d594ea70033
benjamin852 <weinbergben33@gmail.com>
2025-03-31 15:28:28 -0400

    chore: fixed navbar, removed todo

--- src/layouts/navigation.ts
+++ src/layouts/navigation.ts
@@ -104,8 +104,8 @@ export const getNavigation = (section) => {
                 },
                 //leaving xrpl here for now but we'll need to refactor this navigation
                 {
-                  title: "XRPL Interchain Token Service",
-                  href: "/dev/general-message-passing/xrpl/xrpl-gmp/#send-tokens",
+                  title: "XRPL",
+                  href: "/dev/general-message-passing/xrpl/xrpl-gmp/#cross-chain-token-transfer",
                 },
               ],
             },
@@ -241,6 +241,11 @@ export const getNavigation = (section) => {
                       href: "/dev/send-tokens/interchain-tokens/developer-guides/link-custom-tokens-deployed-across-multiple-chains-into-interchain-tokens/",
                     },
                   ],
+                },
+                //leaving xrpl here for now but we'll need to refactor this navigation
+                {
+                  title: "XRPL",
+                  href: "/dev/general-message-passing/xrpl/xrpl-gmp/#cross-chain-token-transfer",
                 },
               ],
             },
