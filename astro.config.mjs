4ccc92563c1d3fb21778789a250c917bf3a1714c
Dharamveer singh <dharamveersingh.em@gmail.com>
2025-05-12 12:38:22 +0530

    feat: add sitemap link to footer and update robots.txt with meta dire....

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,11 @@ export default defineConfig({
     }),
     react(),
     mdx(),
-    sitemap(),
+    sitemap({
+      priority: 0.9,
+      changefreq: "daily",
+      lastmod: new Date(),
+    }),
     tailwind(),
     partytown({
       config: {
