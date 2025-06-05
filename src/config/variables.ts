289530643829d2c46654ff406ff4bac7e50c6cf0
xuan pan <x4pan@uwaterloo.ca>
2025-06-05 19:48:35 -0400

    feat: use global COSMOS_GMP_ADDRESS variable in all docs

--- src/config/variables.ts
+++ src/config/variables.ts
@@ -3,3 +3,4 @@ export const AXELARD_TESTNET = "1.1.2";
 export const AMPD = "1.3.1";
 export const TOFND = "1.0.1";
 export const TOFND_TESTNET = "1.0.1";
+export const COSMOS_GMP_ADDRESS = "axelar1dv4u5k73pzqrxlzujxg3qp8kvc3pje7jtdvu72npnt5zhq05ejcsn5qme5";
